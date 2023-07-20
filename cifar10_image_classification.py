bd350aba388911db88c25cc301b5cae20c52cc28
smomtahe <85276162+smomtahe@users.noreply.github.com>
2023-07-20 07:43:08 -0700

    Create cifar10_image_classification.py

---
+++ cifar10_image_classification.py
@@ -0,0 +1,45 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models, datasets
+import matplotlib.pyplot as plt
+
+# Load the CIFAR-10 dataset
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Define the CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10)
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Train the model
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+# Plot training and validation accuracy over epochs
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# Evaluate the model on the test set
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print(f'Test Accuracy: {test_accuracy}')
+
+# Save the model
+model.save('image_classifier_model.h5')
